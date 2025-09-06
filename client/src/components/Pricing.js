093cd74a2102c6c3774c568b091ed0f0072c5c97
Dinar Davis <dinardavis@gmail.com>
2025-09-06 14:34:44 -0700

    Update payment terms styling

--- client/src/components/Pricing.js
+++ client/src/components/Pricing.js
@@ -59,13 +59,13 @@ export default function Pricing() {
         <div className="site-feature-header">
           <div className="site-feature-col-blank"></div>
           <div className="site-feature-col site-name-container left">
-            <h3 className="site-header-name">Starter Site<br/><span>$1,995***</span></h3>
+            <h3 className="site-header-name">Starter Site<br/><span>$1,995</span></h3>
           </div>
           <div className="site-feature-col site-name-container">
-            <h3 className="site-header-name">Growth Site<br/><span>$3,995***</span></h3>
+            <h3 className="site-header-name">Growth Site<br/><span>$3,995</span></h3>
           </div>
           <div className="site-feature-col site-name-container">
-            <h3 className="site-header-name">Scale Site<br/><span>$5,995***</span></h3>
+            <h3 className="site-header-name">Scale Site<br/><span>$5,995</span></h3>
           </div>
         </div>
 
